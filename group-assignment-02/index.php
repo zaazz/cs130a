955463e6b53647d9be6d9e8e229669540b76d2ad
Greg Gorlen <gsgorlen@gmail.com>
2017-03-15 11:32:45 -0700

    create basic outline

---
+++ group-assignment-02/index.php
@@ -0,0 +1,41 @@
+<?php
+  if (isset($_GET['source'])) {
+    highlight_file($_SERVER['SCRIPT_FILENAME']);
+    exit;
+  }
+?>
+
+<html>
+  <head>
+    <title>Group assignment 02: functios and forms</title>
+    <style>
+      body {
+        font-family: "helvetica";
+        font-size: 15px;
+      }
+      form input {
+        text-align: right;
+      }
+    </style>
+  </head>
+  <body>
+  
+    <?php
+    
+      /**************************/
+      /* php script placeholder */
+      /**************************/
+      
+      // prints a form to collect registration data from a user
+      function displayForm() {
+        // todo: determine variables        
+      }
+      
+      // prints a user's registration information
+      function output(/** parameters here **/) {
+        // todo
+      }
+      
+    ?>    
+  </body>
+</html>
