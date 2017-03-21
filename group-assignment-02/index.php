d00a5f385c208f39b5e123654141a1887699f8c5
Greg Gorlen <gsgorlen@gmail.com>
2017-03-21 15:43:23 -0700

    Flesh out displayRegistrationForm() function
    Added some simple main functionality as well.  Todo: switch get to post?

--- group-assignment-02/index.php
+++ group-assignment-02/index.php
@@ -19,30 +19,84 @@
     </style>
   </head>
   <body>
+    <h1>Sign-up for our thing!</h1>
 
     <?php
 
-      /**************************/
-      /* php script placeholder */
-      /**************************/
-
-      // displays a form to collect registration data from a user
-      function displayForm() {
-        // todo: determine fields and variables
-           // Full name (text)
-           // email
-           // location (City)
-      }
-
-      // prints a user's registration information
-      function output(/** parameters here **/) {
+      // Validates a user's form input
+      function validateRegistration()  {
         // todo
-
+        // use regex for email validation
       }
 
-      function validateForm()  {
+      // Prints a user's registration information
+      function outputRegistration(/** parameters here **/) {
         // todo
       }
+
+      // Sends the pre-set user registration form to the DOM
+      function displayRegistrationForm() {
+        echo "<form method='GET' action='group_assignment02.php'>
+                <table>
+                  <tr>
+                    <td>
+                      First name:
+                    </td>
+                    <td>
+                      <input name=\"firstname\" type=\"text\"
+                        size=15 required autofocus>&nbsp;
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Last name:
+                    </td>
+                    <td>
+                      <input name=\"lastname\" type=\"text\"
+                        size=15 required>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Email:
+                    </td>
+                    <td>
+                      <input name=\"email\" type=\"text\"
+                        size=15 required>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Favorite color:
+                    </td>
+                    <td>
+                      <input name=\"color\" type=\"text\"
+                        size=15 required>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <input type=\"submit\">
+                      <input type=\"reset\">
+                    </td>
+                    <td></td>
+                  </tr>
+                </table>
+              </form>";
+      }
+
+      // Show the user the registration form
+      displayRegistrationForm();
+
+      // If a form has been submitted, process it and display the result
+      if (count($_GET) && isset($_GET["firstname"])) {
+        print "Form submitted...";
+
+        // validateRegistration()
+        // outputRegistration()
+      }
+
     ?>
+
   </body>
 </html>
