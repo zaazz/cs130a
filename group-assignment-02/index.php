6f19dfe369d1c56f963fde87730b5d4fc3d03d2c
tedherr <ted.herr@yahoo.com>
2017-03-22 07:05:07 -0700

    Update index.php

--- group-assignment-02/index.php
+++ group-assignment-02/index.php
@@ -25,14 +25,24 @@
 
       // Validates a user's form input
       function validateRegistration()  {
+          global $firstname,$lastname,$email,$color;
+          $firstname = $_GET['firstname'];
+          $lastname = $_GET['lastname'];
+          $email = $_GET['email'];
+          $color = $_GET['color'];
         // todo
         // use regex for email validation
       }
 
       // Prints a user's registration information
-      function outputRegistration(/** parameters here **/) {
-        // todo
-      }
+  function outputRegistration($firstname,$lastname,$email,$color) {
+      echo "<br><br>Welcome ".$firstname." ! <br>";
+      echo "Your registration information has been recorded as follows..<br>";
+      echo "First name: $firstname<br>";
+      echo "Last name: $lastname<br>";
+      echo "Email: $email<br>";
+      echo "Color: $color<br><br>";
+  }
 
       // Sends the pre-set user registration form to the DOM
       function displayRegistrationForm() {
@@ -91,9 +101,8 @@
       // If a form has been submitted, process it and display the result
       if (count($_GET) && isset($_GET["firstname"])) {
         print "Form submitted...";
-
-        // validateRegistration()
-        // outputRegistration()
+        validateRegistration();
+        outputRegistration($firstname,$lastname,$email,$color);
       }
 
     ?>
