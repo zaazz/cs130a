2b232296271549158d4cb7f499f0e3d784ee4e9b
zaazz <zaazz@users.noreply.github.com>
2017-04-08 17:17:39 -0700

    Revised Square Class
    Changed definition of Square to a Rectangle with same length & width. Updated Index to include the square class & added a square to the canvas.

--- group-assignment-03/square.class.php
+++ group-assignment-03/square.class.php
@@ -8,6 +8,7 @@ include_once('../printable.php');
 class Square extends Rectangle {
     private $size; //Square only requires one length & width
     
+    
     /**
      * Constructor for a Square
      *
@@ -17,52 +18,12 @@ class Square extends Rectangle {
      * @throws InvalidArgumentException
      */
     public function __construct($origin, $size, $color) {
-        $this->setOrigin($origin);
-        $this->setSize($size);
-        $this->setColor($color);
-    }
-
- 
-    /**
-     * Getter for the size of the Square
-     *
-     * @return number size of the Square
-     */
-    public function getSize() {
-        return $this->size;
-    }
-    
-    /**
-     * Setter for the size of the Square
-     *
-     * @param number size of the Square
-     *
-     * @throws InvalidArgumentException
-     */
-    public function setSize($size) {
-        if (is_numeric($size)) {
-            $this->size = $size;
-        }
+       if (is_numeric($size) && $size > 0){
+        Rectangle::__construct($origin, $size, $size, $color);
+           }
         else { throw new InvalidArgumentException(); }
     }
 
-    /**
-     * Returns the area of the Square
-     *
-     * @return number area of the Square
-     */
-    public function getArea() {
-        return pow($this->size, 2);
-    }
-
-    /**
-     * Returns the perimeter of the Square
-     *
-     * @return number perimeter of the Square
-     */
-    public function getPerimeter() {
-        return 4 * ($this->size);
-    }
 } // end Square
 
 ?>
